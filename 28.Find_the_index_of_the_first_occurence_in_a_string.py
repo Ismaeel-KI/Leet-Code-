353988b7445a65e028581b36b96617a6bd50e78a
Ismaeel-KI <kamaalismaeel453@gmail.com>
2025-08-09 08:10:33 +0530

    Added Quesiton to the problems

--- 28.Find_the_index_of_the_first_occurence_in_a_string.py
+++ 28.Find_the_index_of_the_first_occurence_in_a_string.py
@@ -1,14 +1,39 @@
+"""
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+"""
+
 haystack = "leetcode"
 needle = "leeto"
 
-i = 0
-first_occurrence = None
-for j in range(len(haystack)):
-    if haystack[j] == needle[i] and i < len(needle):
-        if first_occurrence is None:
-            first_occurrence = i
-        else:
-            i += 1
-    else:
-        return -1
-return first_occurrence
+found = -1
+if len(haystack) < len(needle):
+    print(-1, "OVER")
+
+else:
+    for start in range(len(haystack) - len(needle) + 1):
+        match = True
+        for j in range(len(needle)):
+            if haystack[start + j] != needle[j]:
+                match = False
+                break
+
+        if match:
+            found = start
+            break
+
+
+    print(found)
